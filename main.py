6dda035ef2d982b50daa1475f64c88cc88a12771
Maxim Bocquillion <bocquillionmaxim@gmail.com>
2025-11-18 11:30:44 +0100

    feat(qlora) : v1 - not tested because no gpu

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from src.utils.utils_main import preprocessing
 from src.data_prep.conversion import dataset_conversion
-import pandas as pd
-import json
+from src.finetuning.qlora.qlora import qlora_finetuning
 from datasets import load_dataset
 
-jsonl_path = "data/preprocessed/recipes_instructions.jsonl"
 
-if __name__ == "__main__":
+def main():
+    jsonl_path = "data/preprocessed/recipes_instructions.jsonl"
     print("Preprocessing")
     # Preprocessing of the Food.com dataset
     data = preprocessing()
@@ -14,8 +13,12 @@ if __name__ == "__main__":
     print("Converting the dataset for model fine tuning")
     dataset_conversion(data)
     dataset = load_dataset("json", data_files=jsonl_path)["train"]
-    # dataset = dataset.train_test_split(test_size=0.05)
-
+    dataset = dataset.train_test_split(test_size=0.05)
+    
     # Finetuning :
     print("Starting the different qwen-05b finetuning process...")
     print("1. QLORA finetuning process...")
+    qlora_finetuning("Qwen/Qwen2.5-0.5B-Instruct", dataset)
+
+if __name__ == "__main__":
+    main()
